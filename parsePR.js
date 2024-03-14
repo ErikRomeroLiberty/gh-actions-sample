b5e54bfb056c1510c59a699e836963e24dd00afc
Erik Romero <erik.romero@outsourcing.libertyseguros.com>
2024-03-14 07:21:43 -0600

    Adicion de template y script para PRs

---
+++ parsePR.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const readLine = require('readline')
+
+async function getTests() {
+	let testsFile = __dirname + '/testsToRun.txt';
+	await fs.promises.writeFile (testsFile,'all');
+	const lines = readLine.createInterface({
+		input: fs.createReadStream (__dirname+'/pr_body.txt'),
+		crlfDelay: Infinity
+	});
+	for await (const line of lines) {
+		if(line.includes('@Tests:')){
+			let tests = line.slice (line.indexOf ('@') + 7);
+			await fs.promises.writeFile (testsFile,tests);
+			await fs.promises.appendFile (testsFile,'\n');
+		}
+	}
+}
+getTests();
